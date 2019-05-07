6014ba1545d87c0a6c006b1fdb59c98b1cc8c223
JuHernandez <47435114+JuHernandez@users.noreply.github.com>
2019-05-07 14:55:11 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+//Spell #1
+
+$("#bibidy").click(function() {
+    $(".dress").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#babidy").click(function() {
+    $(".shoes").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("#buu").click(function() {
+    $(".pimpkin").toggle();
+});
